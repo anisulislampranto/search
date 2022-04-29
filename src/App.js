91e1df24318d18732c81425817d14a83426e23ba
Anisul Islam <anisulislam@Anisuls-MacBook-Pro.local>
2022-04-29 17:58:18 +0600

    onClick site visit added feature

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import { useState } from "react";
 import "./App.css";
 
 const searchedProducts = [
-  { name: "dell supar" },
-  { name: "acer" },
-  { name: "asus" },
-  { name: "apple" },
-  { name: "hp" },
+  { name: "dell", link: "https://en.wikipedia.org/wiki/Dell" },
+  { name: "acer", link: "https://www.acer.com/ac/en/GB/content/home" },
+  { name: "asus", link: "https://www.asus.com/bd/" },
+  { name: "apple", link: "https://www.apple.com/" },
+  { name: "hp", link: "https://www.hp.com/us-en/home.html" },
 ];
 
 function App() {
@@ -16,20 +16,29 @@ function App() {
     <div className="App">
       <h1>Search</h1>
       <input type="text" onChange={(e) => setSearch(e.target.value)} />
-      {searchedProducts
-        .filter((product) => {
-          if (
-            product.name
-              .toLocaleLowerCase()
-              .includes(search.toLocaleLowerCase())
-          ) {
-            return product;
-          }
-          return null;
-        })
-        .map((product, index) => (
-          <li key={index}>{product.name}</li>
-        ))}
+      {search === ""
+        ? null
+        : searchedProducts
+            .filter((product) => {
+              if (
+                product.name
+                  .toLocaleLowerCase()
+                  .includes(search.toLocaleLowerCase())
+              ) {
+                return product;
+              }
+              return null;
+            })
+            .map((product, index) => (
+              <li style={{ listStyleType: "none" }} key={index}>
+                <a
+                  style={{ textDecoration: "none", color: "black" }}
+                  href={product.link}
+                >
+                  {product.name}
+                </a>
+              </li>
+            ))}
     </div>
   );
 }
